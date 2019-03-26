9da3530fab2a5070fd6516846303e5bb2e8abe4e
Komstuch <komstuchxbox@gmail.com>
2019-03-26 21:48:21 +0100

    Add movement to the Attacker

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0f,2f)]
+    [SerializeField] float movementSpeed = .7f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * movementSpeed);
+        
+    }
+}
