80096b36777e20ef103e95a8287586052395bbad
Komstuch <komstuchxbox@gmail.com>
2019-03-26 22:22:05 +0100

    Create Attacker Spawneer Scrit

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    bool spawn = false;
+    [SerializeField] float minTimeBetweenSpawn = 3f;
+    [SerializeField] float maxTimeBetweenSpawn = 5f;
+    [SerializeField] float timeToSpawn;
+    [SerializeField] GameObject attacker;
+
+    IEnumerator Start()
+    {
+        timeToSpawn = UnityEngine.Random.Range(minTimeBetweenSpawn, maxTimeBetweenSpawn);
+        yield return new WaitForSeconds(timeToSpawn);
+        SpawnAttacker();
+        spawn = true;
+    }
+
+    private void SpawnAttacker()
+    {
+        GameObject currentAttacker = Instantiate(attacker, transform.position, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        if (spawn)
+        {
+            spawn = false;
+            StartCoroutine(Start());
+        }
+    }
+}
