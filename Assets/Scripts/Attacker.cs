bbb0c26dfe3c4d6ea783a8016b5c97709f875f5b
Komstuch <komstuchxbox@gmail.com>
2019-03-27 16:31:36 +0100

    Projectile deal damage and destroy atackers

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -6,6 +6,8 @@ public class Attacker : MonoBehaviour
 {
     float movementSpeed = 0f;
 
+    [SerializeField] float health;
+
     void Update()
     {
         transform.Translate(Vector2.left * Time.deltaTime * movementSpeed);  
@@ -14,5 +16,21 @@ public class Attacker : MonoBehaviour
     public void SetMovementSpeed (float speed)
     {
         movementSpeed = speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collided with smth");
+        Projectile projectile = collision.gameObject.GetComponent<Projectile>();
+        if (projectile)
+        {
+            float damageRecieved = projectile.GetDamage();
+            health -= damageRecieved;
+            if(health <= 0)
+            {
+                Debug.Log("Attacker: " + gameObject.name + " is dead!");
+                Destroy(gameObject);
+            }
+        }
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,9 +6,24 @@ public class Projectile : MonoBehaviour
 {
     [Range (0f,10f)]
     [SerializeField] float projectileSpeed;
+    [SerializeField] float projectileDamage;
 
     void Update()
     {
         transform.Translate(Vector2.right * Time.deltaTime * projectileSpeed);           
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var health = collision.GetComponent<Health>();
+        if (health) health.DealDamage(projectileDamage);
+
+        if (collision.gameObject.GetComponent<Attacker>())
+        {
+            Debug.Log("collided with the attcker");
+            Destroy(gameObject);
+        }
+    }
+
+    public float GetDamage() { return projectileDamage; }
 }
