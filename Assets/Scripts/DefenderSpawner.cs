dab683230aa448540eec640bcb0947ea5b5e2e63
Komstuch <komstuchxbox@gmail.com>
2019-03-27 21:50:52 +0100

    Spawn defender on the play space (5x7)

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+    private void OnMouseDown()
+    {
+        SpawnDefender();
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return worldPos;
+    }
+
+    private void SpawnDefender()
+    {
+        GameObject newDefender = Instantiate(defender, GetSquareClicked(), Quaternion.identity) as GameObject;
+    }
+}
