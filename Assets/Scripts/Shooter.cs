f08a22e3fdfa08e27fa068e99c81610b65a12b75
Komstuch <komstuchxbox@gmail.com>
2019-03-27 14:10:11 +0100

    Create Zealot Defender &  instantiate projectile during his attack

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    [SerializeField] GameObject projectile;
+    [SerializeField] GameObject gun;
+
+    public void Fire(float damage)
+    {
+        Instantiate(projectile, gun.transform.position, Quaternion.identity);
+        return;
+    }
+}
