7d84de56e169c8953d0b5a9fa87480a3555517fc
Komstuch <komstuchxbox@gmail.com>
2019-03-27 21:25:44 +0100

    Add many Audio Clips

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,11 +7,16 @@ public class Health : MonoBehaviour
 
     [SerializeField] float health = 100f;
 
+    [SerializeField] AudioClip hitSound;
+    [SerializeField] AudioClip deathSound;
+
     public void DealDamage(float damage)
     {
+        AudioSource.PlayClipAtPoint(hitSound, transform.position, 0.5f);
         health -= damage;
         if (health <= 0)
         {
+            AudioSource.PlayClipAtPoint(deathSound, transform.position, 0.5f);
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -8,6 +8,8 @@ public class Shooter : MonoBehaviour
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject gun;
 
+    [SerializeField] AudioClip shootingSound;
+
     public void Fire(float damage)
     {
         Instantiate(projectile, gun.transform.position, Quaternion.identity);
@@ -18,5 +20,10 @@ public class Shooter : MonoBehaviour
         GetComponent<Animator>().speed = 0;
         yield return new WaitForSeconds(time);
         GetComponent<Animator>().speed = 1;
+    }
+
+    public void PlaySound()
+    {
+        AudioSource.PlayClipAtPoint(shootingSound, transform.position, 0.5f);
     }
 }
