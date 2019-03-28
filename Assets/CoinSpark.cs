cfc6d60c6f485f06e2762fea6a135013b8c5d07f
Komstuch <komstuchxbox@gmail.com>
2019-03-28 17:35:08 +0100

    Coins are generated by the gold mine + additional VFX

---
+++ Assets/CoinSpark.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpark : MonoBehaviour
+{
+    [SerializeField] GameObject coinVFX;
+
+    public void Spark()
+    {
+        GameObject VFX = Instantiate(coinVFX, transform.position, Quaternion.identity) as GameObject;
+        Destroy(VFX, 1f);
+    }
+}
--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -5,4 +5,10 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
     [SerializeField] float cost;
+
+    public void AddCoins(int coins)
+    {
+        FindObjectOfType<CoinDisplay>().AddCoins(coins);
+        GetComponentInChildren<CoinSpark>().Spark(); // Play spark vfx
+    }
 }
