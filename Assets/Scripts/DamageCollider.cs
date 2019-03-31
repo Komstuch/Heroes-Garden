bdc9a40696161d6bcf41301e6364208b2274aa27
Komstuch <komstuchxbox@gmail.com>
2019-03-31 19:45:45 +0200

    Display lives. ability to lose game and lose screen

---
+++ Assets/Scripts/DamageCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageCollider : MonoBehaviour
+{
+    Lives lives;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        lives = FindObjectOfType<Lives>();
+        lives.TakeLife();
+    }
+}
