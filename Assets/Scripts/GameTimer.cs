488dca13f1a1b80d29db1a53cd0f51a9820c88e2
Komstuch <komstuchxbox@gmail.com>
2019-04-20 15:54:36 +0200

    Create Animated Slider Background

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("Game time in seconds")]
+    [SerializeField] float levelTime = 10f;
+
+    void Update()
+    {
+        float percentComplete = Time.timeSinceLevelLoad / levelTime;
+        GetComponent<Slider>().value = percentComplete;
+
+        bool levelComplete = (Time.timeSinceLevelLoad > levelTime);
+
+        if (levelComplete) Debug.Log("Debug Timer Expierd");
+    }
+}
