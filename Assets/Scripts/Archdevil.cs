97516618563e9beeb26859afdc10417cc007d704
Komstuch <komstuchxbox@gmail.com>
2019-05-20 16:40:14 +0200

    Archdevil move and attack Pattern

--- Assets/Scripts/Archdevil.cs
+++ Assets/Scripts/Archdevil.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Archdevil : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator animator;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetTrigger(string trigger)
     {
-        
+        animator.SetTrigger(trigger);
+    }
+
+    public void MoveLeft()
+    {
+        int newYPosition = UnityEngine.Random.Range(1, 6);
+        float newXPosition = transform.position.x-1f;
+ 
+        transform.position = new Vector2(newXPosition, newYPosition);
+        animator.SetTrigger("AD_Reappear");
+        SwitchLanes(newYPosition);
+    }
+
+    private void SwitchLanes(int yPos)
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+        foreach(AttackerSpawner spawner in spawners)
+        {
+            if(yPos == spawner.transform.position.y)
+            {
+                transform.parent = spawner.transform;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject otherObject = collision.gameObject;
+
+        if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
     }
 }
